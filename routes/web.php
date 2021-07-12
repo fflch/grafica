89658a97b96a2ebba135d60c116f79bb1a640a17
Augusto César Freire Santiago <acesarfs@usp.br>
2021-07-12 09:30:29 -0300

    Adicionada opção de Log

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,6 @@ Route::get('/settings',[GeneralSettingsController::class, 'show']);
 Route::post('/settings',[GeneralSettingsController::class, 'update']);
 
 Route::get('acesso/autorizado', [PedidoController::class,'acesso_autorizado'])->name('acesso_autorizado');
+
+# Logs  
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index')->middleware('can:admin');

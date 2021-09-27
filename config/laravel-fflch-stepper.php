ea0e78e24e95aeb29f4ae83b6398368623171b83
Victor Oliveira <vitor.oliveira.marinho@gmail.com>
2021-09-27 10:37:39 -0300

    atualizaçao stepper

---
+++ config/laravel-fflch-stepper.php
@@ -0,0 +1,34 @@
+<?php
+
+return [ 
+    'steps' => [
+        'Em Elaboração' => [
+            'icon'     => '<i class="far fa-plus-square"></i>',
+            'description' => ''
+        ],
+        'Em Análise' => [
+            'icon'     => '<i class="fas fa-glasses"></i>',
+            'description' => ''
+        ],
+        'Orçamento' => [
+            'icon'     => '<i class="fas fa-user-clock"></i>',
+            'description' => ''
+        ],
+        'Autorização' => [
+            'icon'     => '<i class="fas fa-user-lock"></i>',
+            'description' => ''
+        ],
+        'Editoração' => [
+            'icon'     => '<i class="fas fa-newspaper"></i>',
+            'description' => ''
+        ],
+        'Artes Gráficas' => [
+            'icon'     => '<i class="fas fa-print"></i>',
+            'description' => ''
+        ],
+        'Finalizado' => [
+            'icon'     => '<i class="fas fa-clipboard-check"></i>',
+            'description' => ''
+        ],
+    ]
+];

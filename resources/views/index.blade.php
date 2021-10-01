47b24ec1ed3d8f3c9fb8ba9cb5dd9b9b163550c9
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-09-30 22:33:24 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 
-Sistema Gráfica - FFLCH
+Sistema para solicitação de serviços da Editora e Gráfica - FFLCH
+
+<br><br>
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/8VSvUe_r4dA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
